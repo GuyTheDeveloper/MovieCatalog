737fc2a5e634078424d65b53cd59bf9814bf8379
GuyTheDeveloper <goldenrodge99@gmail.com>
2022-03-29 20:26:01 +0500

    Added modal, bookmark

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const closeMovie = document.querySelector('.close-btn');
-const movie = document.querySelector('.movie')
+const movie = document.querySelector('.movie');
 const closeSecond = document.querySelector('.close--btn');
 const movieSecond = document.querySelector('.list--item');
 
@@ -10,4 +10,29 @@ closeMovie.addEventListener('click', () => {
 
 closeSecond.addEventListener('click', () => {
     movieSecond.classList.add('d-none');
+})
+
+const btn = document.getElementById('btn');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close');
+
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+closeBtn.onclick =  function () {
+    modal.style.display = "none";
+}
+
+const mark = document.getElementById('bookmark');
+
+mark.addEventListener('click', () => {
+    movie.classList.remove('d-none');
+})
+
+const markSecond = document.getElementById('mark');
+
+markSecond.addEventListener('click', () => {
+    movieSecond.classList.remove('d-none');
 })
